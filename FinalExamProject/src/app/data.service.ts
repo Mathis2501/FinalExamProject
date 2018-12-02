c3b9e2a054d2de2591762fd6b1b33fcb8c945c98
Mathias Bellerby Sylvester Nielsen <math130u@edu.eal.dk>
2018-12-02 18:10:24 +0100

    OpenCV4NodeJS initial commit

--- FinalExamProject/src/app/data.service.ts
+++ FinalExamProject/src/app/data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import * as Tesseract from '../../node_modules/tesseract.js/dist/tesseract.js';
+import * as cv from 'opencv4nodejs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,11 @@ export class DataService {
   }
 
   handleImage(image: Tesseract.ImageLike) {
+
+    // const mat = cv.imread(image);
+    // const gray = mat.bgrToGray();
+    // cv.imwrite('./img.png', gray);
+
     Tesseract.recognize(image)
     .progress(message => console.log(message))
     .catch(err => console.error(err))
