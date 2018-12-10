b9a1bd24efe6e681a7e1e97db3e4818360c4a620
Casper Ludvigsen <casp6239@edu.eal.dk>
2018-12-10 16:01:52 +0100

    made something to uploadFile

--- src/app/home/home-view/home-view.component.ts
+++ src/app/home/home-view/home-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-home-view',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-view.component.css']
 })
 export class HomeViewComponent implements OnInit {
+selectedFile: File = null;
 
-  constructor() { }
+  constructor(private httpclient: HttpClient) { }
 
   ngOnInit() {
+  }
+
+  onFIleSelected(event){
+    this.selectedFile = <File>event.target.files[0];
+
+  }
+
+  UploadFile(){
+    const fd = new FormData;
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Ocp-Apim-Subscription-Key': '764da9f070ad481d87ce80f1b50c2fdc'
+      })
+    }
+    fd.append('image', this.selectedFile, this.selectedFile.name)
+    this.httpclient.post('https://northeurope.api.cognitive.microsoft.com/vision/v1.0/ocr?language=unk&detectOrientation =true', this.selectedFile, httpOptions).subscribe(r => {console.log(r)});
   }
 
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeViewComponent } from './home-view/home-view.component';
+import {HttpClientModule} from '@angular/common/http'
 
 @NgModule({
   declarations: [HomeViewComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
   ]
 })
 export class HomeModule { }
