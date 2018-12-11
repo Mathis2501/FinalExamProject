42ee806e6db45482e3cd5baf760fbf8cb05647be
Mathias Bellerby Sylvester Nielsen <math130u@edu.eal.dk>
2018-12-11 17:20:21 +0100

    we can now loop through ocr return values

--- src/app/home/home-view/home-view.component.ts
+++ src/app/home/home-view/home-view.component.ts
@@ -14,20 +14,36 @@ selectedFile: File = null;
   ngOnInit() {
   }
 
-  onFileSelected(event){
+  onFileSelected(event) {
     this.selectedFile = <File>event.target.files[0];
   }
 
-  UploadFile(){
+  UploadFile() {
     const fd = new FormData;
     const httpOptions = {
       headers: new HttpHeaders({
         'Ocp-Apim-Subscription-Key': '764da9f070ad481d87ce80f1b50c2fdc'
-      })
-    }
-    fd.append('image', this.selectedFile, this.selectedFile.name)
+      }),
+    };
+    fd.append('image', this.selectedFile, this.selectedFile.name);
+    // tslint:disable-next-line:max-line-length
     this.httpclient.post<any>('https://northeurope.api.cognitive.microsoft.com/vision/v2.0/ocr?language=unk&detectOrientation =true', fd, httpOptions)
-      .subscribe(response => {console.log(response)});
-  }
-
+    .subscribe(
+      (val) => {
+        console.log(val);
+        val.regions.forEach(region => {
+          region.lines.forEach(line => {
+            line.words.forEach(word => {
+              console.log(word.text);
+            });
+          });
+        });
+      },
+      response => {
+        console.log('POST call in error', response);
+    },
+    () => {
+        console.log('The POST observable is now completed.');
+    });
+    }
 }
